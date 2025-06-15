c97922c5c90520c90858bfdbda99a85e6671efdd
loaamiera <brownamiera@gmail.com>
2025-06-15 13:13:19 -0700

    Add files via upload

---
+++ location.js
@@ -0,0 +1,27 @@
+
+
+var locScript = document.getElementById("Location");
+
+locScript.onclick = function Location () {
+
+    var ponderScript = locScript
+    ponderScript.onclick = function pondering () {
+        import java.awt
+        navigator.geolocation() ;
+        ponderScript.innerhtml = "Time to ponder!" ;
+        
+        Location.innerhtml.onclick = function (water){
+            alert('Look at the water as you breathe.') ;
+            Location.innerhtml.onclick = function (quote1) {
+                "blach blach "
+                Location.innerhtml.onlick.setText(getText()) = function (dialogue) { 
+                    "How did it go?"
+                } ;
+            
+            } ;
+
+            } ;
+        } ;
+
+
+        } ;
