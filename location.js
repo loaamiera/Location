d7d60765e2023144c8d2963fc0d8ebcd31f772d5
loaamiera <brownamiera@gmail.com>
2025-06-15 17:08:00 -0700

    Update location.js

--- location.js
+++ location.js
@@ -1,15 +1,15 @@
 
-import java.awt ;
-
-var locScript = document.getElementById("Location");
 document.addEventListener('DOMContentLoaded', () => {
+import java.awt ;
+var locScript = document.getElementById("Location");
 locScript.onclick = function Check() {
   if (navigator.geolocation) {
   var userLoc = navigator.geolocation.getCurrentPosition(Location);
 } 
 else {
-  document.getElementById("Location").innerHTML =
-  "Please make sure that geolocation is turned on.";
+  locScript.textContent = ''
+  locScript.textContent =
+  "Please make sure that your location services are turned on.";
    var userLoc = navigator.geolocation.watchPosition(Check) ;
 } ;
 } ;
@@ -17,22 +17,27 @@ else {
  start = function Location(position) {
     var Latitude = position.coords.latitude;
     var Longitude = position.coords.longitude;
+   locScript.textContent = ''
     if(Latitude != 48.729321 OR Longitude != -122.485973) {
-        locScript.innerhtml = "Someone's not at the Fairhaven pond! Please make your way there before you click." ;
+        locScript.innerhtml = "Someone's not at the Fairhaven pond! Please make you're already there before you click to continue." ;
         var userLoc = navigator.geolocation.watchPosition(Check) ;
     } 
     else {
-        var pondScript = locScript.innerhtml ;
-        pondScript = "Time to ponder! When done, please click to continue" ;  
+        var pondScript = ' '
+        var locScript = ' '
+        pondScript.createElement('p') ;
+        pondScript.p.innerhtml = "Time to ponder! When done, please click to continue" ;  
         pondScript.onclick = function water (){
-            pondScript = 'Look at the water as you breathe. When calm, please click to continue.' ;
-            pondscript.onclick = function quote () {
-                "blach blach "
-                pondScript.setText(getText()) = function (dialogue) { 
+          timeElapsed = performance.now() ;
+            pondScript.p.innerhtml = 'Look at the water as you breathe. When calm, please click to continue.' ;
+            pondscript.onclick = if (timeElapsed < 120000 ) {
+            pondScript.p.innerhtml = '' ;
+            pondScript.p.innerhtml = 'Someones in a hurry. Take a few minutes to think!' ;
+            }
+           else { nextPart = unction (dialogue) { 
                     section = document.getElementById('toSend') ;
-                    section.innerHtml = 'How did it go? As a warning, I will be receiving your response.'
-                    textScript = pondScript.createElement('input') ;
-                    section.innerHtml.append(textScript) ;
+                    section.createElement('input') ;
+                    section.input.innerHtml = 'How did it go? As a warning, I will be receiving your response.'
                     .then(response => sendEmail()) ;
                       function sendEmail() {
                           const emailData = {
