e59426d2b7cdcea2e10b6b261d2cc7c666f7ee10
loaamiera <brownamiera@gmail.com>
2025-06-15 21:24:43 -0700

    Update location.js

--- location.js
+++ location.js
@@ -1,61 +1,60 @@
+const locScript = document.getElementById("Location");
 
-
-var locScript = document.getElementById("Location");
-locScript.onclick = function Check() {
+locScript.onclick = function checkLocation() {
   if (navigator.geolocation) {
-  var userLoc = navigator.geolocation.getCurrentPosition(Location);
-} 
-else {
-  locScript.innerhtml =
-  "Please make sure that your location services are turned on.";
-   var userLoc = navigator.geolocation.watchPosition(Check) ;
-} 
-} ;
- start = function Location(position) {
-    var Latitude = position.coords.latitude;
-    var Longitude = position.coords.longitude;
-    if(Latitude != 48.729321) {
-        locScript.innerhtml = "Someone's not at the Fairhaven pond! Please make you're already there before you click to continue." ;
-        var userLoc = navigator.geolocation.watchPosition(Check) ;
-    } 
-   else{
-     if (Longitude != -122.485973) {
-        locScript.innerhtml = "Someone's not at the Fairhaven pond! Please make you're already there before you click to continue." ;
-        var userLoc = navigator.geolocation.watchPosition(Check) ; }
-      else {
-        locScript.innerhtml = "Time to ponder! When done, please click to continue" ;  
-        locScript.onclick = function water (){
-          timeElapsed = performance.now() ;
-            locScript.innerhtml = 'Look at the water as you breathe. When calm, please click to continue.' ;
-            locScript.onclick = function pondTime() {
-              if (timeElapsed < 120000 ) {
-              locScript.innerhtml = '' ;
-              locScript.innerhtml = 'Someones in a hurry. Take a few minutes to think!' ;
-            }
-           else { nextPart = function (dialogue) { 
-                    section = document.getElementById('toSend') ;
-                    section.createElement('input') ;
-                    section.createElement('label') ;
-                    section.label.textContent = 'How did it go? As a warning, I will be receiving your response.' ;
-                       }.then((response) => { return response.json() ; } )
-                         .then (data => {
-                      function sendEmail() {
-                          const emailData = {
-                                to: 'amierasdocs@gmail.com',
-                                subject: 'Pondering Response!',
-                                text: data }
-                          } 
-                      fetch('/send-email', {
-                        method: 'POST',
-                        headers: { 'Content-Type': 'application/json' },
-                        body: JSON.stringify(emailData)
-                        }).then(response => response.json())
-                        .then(data => console.log(data));
-                                                          }  )
-            
+    navigator.geolocation.getCurrentPosition(handleLocation);
+  } 
+  else {
+    locScript.innerHTML = "Please make sure that your location services are on to use this website.";
+  }
+};
 
-            } ;
-        } ;
+function checkLocation(position) {
+  const { latitude, longitude } = position.coords;
+  const idealLatitude = 48.729321;
+  const idealLongitude = -122.485973;
 
+  if (latitude != targetLat || longitude != idealLong) {
+    locScript.innerHTML = Looks like someones not at the Fairhaven dorm pond!";
+    navigator.geolocation.watchPosition(checkLocation);
+    return;
+  }
 
-} } ; } ; } ; 
+  else {
+    var startTime = Date.now();
+    locScript.innerHTML = "Time to ponder! When done, click to continue.";
+    locScript.onclick = function water() {
+    const timeElapsed = Date.now() - startTime;
+    if (timeElapsed < 120000) {
+       locScript.innerHTML = "Someone's in a hurry! Look at the water as you breathe. When fully calm, click to continue "; }
+    else {
+      var section = document.getElementById("toSend");
+      locScript.innerHTML = 
+
+        var label = document.createElement("label");
+        label.textContent = "So, how was it? Just to warn you I will be receiving your response.";
+        section.appendChild(label);
+        var input = document.createElement("input");
+        input.type = "text";
+        section.appendChild(input);
+        var.onSubmit  () => {
+          const emailData = {
+            to: "amierasdocs@gmail.com",
+            subject: "Pondering Response!",
+            text: input.value
+          };
+
+          fetch("/send-email", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(emailData)
+          })
+          .then(response => response.json())
+          .then(data => {
+            locScript.innerHTML = "I hope this helped! That's all the java that I've written for this.";
+          });
+        };
+      }
+    };
+  };
+}
